41687e8d94541446c346dd604cbb28d91f6e7864
Daniel <danielmordelfer@gmail.com>
2023-03-31 13:23:00 +0100

    scnd stopwatch and getting the value for each stopwatch

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -11,6 +11,13 @@ namespace Tempo
             Stopwatch crono1 = new Stopwatch();
             crono1.Start();
             Thread.Sleep(500);
+            Stopwatch crono2 = new Stopwatch();
+            Thread.Sleep(300);
+            crono1.Stop();
+            crono2.Stop();
+
+            TimeSpan ts1 = crono1.Elapsed;
+            TimeSpan ts2 = crono2.Elapsed;
         }
     }
 }
