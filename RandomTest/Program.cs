21ba6cabe37069e338e845d1cf4e8e82fb97e603
Daniel <danielmordelfer@gmail.com>
2023-03-31 13:59:30 +0100

    test

---
+++ RandomTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RandomTest
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random r1 = new Random(23);
+            Random r2 = new Random(22);
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r1.Next());
+            Console.WriteLine();
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r2.Next());
+        }
+    }
+}
