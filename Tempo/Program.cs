e8263f280fb9270b8460f730b25b6f35f2fd2705
Daniel <danielmordelfer@gmail.com>
2023-03-31 13:49:52 +0100

    Printing the elapsed time of each stopwatch

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -12,12 +12,22 @@ namespace Tempo
             crono1.Start();
             Thread.Sleep(500);
             Stopwatch crono2 = new Stopwatch();
+            crono2.Start();
             Thread.Sleep(300);
             crono1.Stop();
             crono2.Stop();
 
             TimeSpan ts1 = crono1.Elapsed;
             TimeSpan ts2 = crono2.Elapsed;
+
+            string elapsedTime1 = String.Format("{0:}.{1:00}",
+                ts1.Seconds, ts1.Milliseconds / 10);
+
+            string elapsedTime2 = String.Format("{0:}.{1:00}",
+                ts2.Seconds, ts2.Milliseconds / 10);
+
+            Console.WriteLine("RunTime of crono 1: " + elapsedTime1+"s");
+            Console.WriteLine("RunTime of crono 2: " + elapsedTime2+"s");
         }
     }
 }
