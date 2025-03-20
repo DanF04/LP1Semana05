241465ca33b03bf666f40628015fd4b818214008
Daniel <danielmordelfer@gmail.com>
2025-03-20 13:02:34 +0000

    Add color print

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -14,8 +14,8 @@ namespace LevelOne
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
-            Console.WriteLine("Hello LP1!");
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Nice day!");
+            Console.WriteLine("Bye!");
         }
         /// <summary>
         /// Method that returns nothing
