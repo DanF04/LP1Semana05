e662ef69a8546c8fb8f97792c55404e2d8d72601
Daniel <danielmordelfer@gmail.com>
2025-03-20 12:37:05 +0000

    Add initial code

---
+++ LevelOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LevelOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
