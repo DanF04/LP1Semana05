011105d1fddc886dad5d59ff0b1bd8321d66ac0b
Daniel <danielmordelfer@gmail.com>
2025-03-20 13:00:06 +0000

    Add two prints

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -14,6 +14,8 @@ namespace LevelOne
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Hello LP1!");
         }
         /// <summary>
         /// Method that returns nothing
