b32294512e8dd16569ddb746f07eedb4858cd00b
Daniel <danielmordelfer@gmail.com>
2025-03-21 00:27:27 +0000

    New program that prints a message

---
+++ SoGoodLib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoGoodLib
+{
+    public class SoGoodClass
+    {
+        public static string SoGoodMethod()
+        {
+            return "I came from another project!";
+        }
+    }
+}
